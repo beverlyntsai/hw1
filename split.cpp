da98553e987f63d4fa2ddc7a72d418e21dac1d75
Beverlyn Tsai <beverlyn@usc.edu>
2025-09-05 23:51:41 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -19,24 +19,37 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  // Create base case where in is null
   if(in == NULL){
+    // Set odds and evens linked list to null
     odds = NULL;
     evens = NULL;
     return;
   }
 
+  // Create current pointer to walk through the code instead of in
   Node* curr = in;
+  // Move in to next
   in = in->next;
+  // Create recursive case
   split(in, odds, evens);
 
+  // End the new linked list with null
   curr->next = NULL;
 
+  // Check to see if value is odd
   if(curr->value % 2 == 1){
+    // Update current pointer to the head of odds linked list
     curr->next = odds;
+    // Update odds pointer to point to current, the new head
     odds = curr;
   }
+  // Check to see if value is even
   else{
+    // Update current pointer to the head of evens linked list
     curr->next = evens;
+    // Update evens pointer to point to current, the new head
     evens = curr;
   }
 }
