fa1988b0cd19258034000ace350b8322f5b8ab84
Beverlyn Tsai <beverlyn@usc.edu>
2025-09-05 22:55:01 +0000

    all done with part 1

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,34 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+
+// Helper display function
+void display(Node* head){
+  Node* curr = head;
+  while(curr != NULL){
+    std::cout << curr->value << std::endl;
+    curr = curr->next;
+  }
+}
 
 int main(int argc, char* argv[])
 {
+  Node* in = new Node{1, nullptr};
+  in->next = new Node{2, nullptr};
+  in->next->next = new Node{3, nullptr};
+  in->next->next->next = new Node{4, nullptr};
 
+  Node* odds = NULL;
+  Node* evens = NULL;
+
+  split(in, odds, evens);
+
+  // Display new linked lists
+  std::cout << "Odd list: " << std::endl;
+  display(odds);
+
+  std::cout << "Even list: " << std::endl;
+  display(evens);
 }
